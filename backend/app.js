aa152fbca906bc3d4e40989fc9e7b319f7b225df
Alex Vilentchuk <avilentchuk@gmail.com>
2022-01-31 06:01:05 +0200

    2.1

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const requestStamp = require('./middleware/requestStamp');
 const {reqLogger:log} = require('./scripts/logging');
 const { login, createUser } = require('./controllers/users');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
