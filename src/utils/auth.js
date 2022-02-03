e7cadcab76d6979ec85285154cb2b3004af995c5
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-03 02:30:58 +0200

    test

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://api.avilentchuk3.students.nomoreparties.sbs";
-// export const BASE_URL = "http://127.0.0.1:3000"; local
+
 const checkResponse = (response) => {
   if (response.ok) return response.json();
   return Promise.reject(response);
