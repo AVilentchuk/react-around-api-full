c00af690f68c0c097df202727822629cbc7a2dbd
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-04 19:14:39 +0200

    test

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -16,16 +16,17 @@ module.exports.updateUser = celebrate({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri()
-    }).unknown(true)
+    })
+    .unknown(true)
     .error(() => Promise.reject(invalidRequest))
 });
-
 
 module.exports.card = celebrate({
   body: Joi.object()
     .keys({
       name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().uri()
-    }).unknown(true)
+    })
+    .unknown(true)
     .error(() => Promise.reject(invalidRequest))
 });
