d838e63499bb78ffd8a9778a2e958974fdf35495
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-04 19:08:55 +0200

    validation added for routre body pre request

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -5,29 +5,27 @@ module.exports.sign = celebrate({
   body: Joi.object()
     .keys({
       email: Joi.string().required().min(6).email(),
-      password: Joi.string().required().min(2),
+      password: Joi.string().required().min(2)
     })
     .error(() => Promise.reject(invalidRequest))
 });
 
-module.exports.user = celebrate({
+module.exports.updateUser = celebrate({
   body: Joi.object()
     .keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri(),
-      _id: Joi.string().hex().length(24).required(),
-    })
-    .unknown(true)
+      avatar: Joi.string().uri()
+    }).unknown(true)
     .error(() => Promise.reject(invalidRequest))
 });
+
 
 module.exports.card = celebrate({
   body: Joi.object()
     .keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().uri(),
-    })
-    .unknown(true)
+      link: Joi.string().required().uri()
+    }).unknown(true)
     .error(() => Promise.reject(invalidRequest))
 });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.get('/', getUsers);
 router.get('/me', getSelf);
 router.get('/:id', getUser);
-router.post('/',userValidator, createUser);
+router.post('/', userValidator, createUser);
 router.patch('/me', userValidator, updateProfile);
 router.patch('/me/avatar',userValidator, updateAvatar);
 
