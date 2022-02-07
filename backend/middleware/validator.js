07800f55355726324e3d4ac15cd1916d6950dfad
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-07 21:15:18 +0200

    ..

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -1,10 +1,11 @@
 const { celebrate, Joi } = require('celebrate');
+const { validationError }= require('../constants/errors');
 
 module.exports.sign = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(6).email(),
     password: Joi.string().required().min(2)
-  })
+  }).error(()=>{return Promise.reject(validationError)})
 });
 
 module.exports.updateUser = celebrate({
@@ -17,19 +18,19 @@ module.exports.updateUser = celebrate({
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri(),
       _id: Joi.string().hex().length(24)
-    })
+    }).error(()=>{return Promise.reject(validationError)})
     .unknown(true)
 });
 
 module.exports.card = celebrate({
   params: Joi.object({
     id: Joi.string().hex().length(24)
-  }).unknown(true),
+  }).unknown(true).error(()=>{return Promise.reject(validationError)}),
   body: Joi.object()
     .keys({
       name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().uri(),
       _id: Joi.string().hex().length(24)
     })
-    .unknown(true)
+    .unknown(true).error(()=>{return Promise.reject(validationError)})
 });
