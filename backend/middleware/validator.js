504e510008db31f9f8082447e0b563c08d279cd0
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-07 04:27:35 +0200

    t3

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -3,31 +3,27 @@ const errorHandler = require('../scripts/errorHandler');
 const { invalidRequest } = require('../constants/errors');
 
 module.exports.sign = celebrate({
-  body: Joi.object()
-    .keys({
-      email: Joi.string().required().min(6).email(),
-      password: Joi.string().required().min(2),
-    })
-    .error(() => Promise.reject(invalidRequest)),
+  body: Joi.object().keys({
+    email: Joi.string().required().min(6).email().error(invalidRequest),
+    password: Joi.string().required().min(2).error(invalidRequest),
+  }),
 });
 
 module.exports.updateUser = celebrate({
   body: Joi.object()
     .keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri(),
+      name: Joi.string().min(2).max(30).error(invalidRequest),
+      about: Joi.string().min(2).max(30).error(invalidRequest),
+      avatar: Joi.string().uri().error(invalidRequest),
     })
-    .error(() => Promise.reject(invalidRequest))
     .unknown(true),
 });
 
 module.exports.card = celebrate({
   body: Joi.object()
     .keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().uri(),
+      name: Joi.string().required().min(2).max(30).error(invalidRequest)s,
+      link: Joi.string().required().uri().error(invalidRequest),
     })
-    .error(() => Promise.reject(invalidRequest))
     .unknown(true),
 });
