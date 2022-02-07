03474511ee48d5f836a383ef2509d8d9eb7fc12d
Alex Vilentchuk <avilentchuk@gmail.com>
2022-02-07 04:47:33 +0200

    3

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -8,7 +8,7 @@ module.exports.sign = celebrate({
       email: Joi.string().required().min(6).email(),
       password: Joi.string().required().min(2),
     })
-    .error(() => errorHandler(req, req, invalidRequest)),
+    .error(() => Promise.reject(invalidRequest)),
 });
 
 module.exports.updateUser = celebrate({
@@ -18,7 +18,7 @@ module.exports.updateUser = celebrate({
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri(),
     })
-    .error(() => errorHandler(req, req, invalidRequest))
+    .error(() => Promise.reject(invalidRequest))
     .unknown(true),
 });
 
@@ -28,6 +28,6 @@ module.exports.card = celebrate({
       name: Joi.string().required().min(2).max(30),
       link: Joi.string().required().uri(),
     })
-    .error(() => errorHandler(req, req, invalidRequest))
+    .error(() => Promise.reject(invalidRequest))
     .unknown(true),
 });
